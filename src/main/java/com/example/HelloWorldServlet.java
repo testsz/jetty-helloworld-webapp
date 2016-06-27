5bc5534e57612685db6bf700485e679bc5f47084
testsz <testsz@users.noreply.github.com>
2016-06-26 17:03:13 -0700

    Update HelloWorldServlet.java

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -1,18 +1,18 @@
-package com.example;
+package org.example;
 
-import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
-public class HelloWorldServlet extends HttpServlet 
+public class HelloServlet extends HttpServlet
 {
-  @Override 
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException
-  {
-      response.getOutputStream().print("Hello World");
-  }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Hello Servlet</h1>");
+        response.getWriter().println("session=" + request.getSession(true).getId());
+    }
 }
